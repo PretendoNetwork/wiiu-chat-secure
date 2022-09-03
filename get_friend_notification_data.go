fc3c4b3bfbc68f6ded307cc6ef4a2dd63ed5fad8
Jonathan Barrow <jonbarrow1998@gmail.com>
2022-09-03 01:07:59 -0400

    Removed unneeded NotificationEvent structure

--- get_friend_notification_data.go
+++ get_friend_notification_data.go
@@ -8,27 +8,6 @@ import (
 	nexproto "github.com/PretendoNetwork/nex-protocols-go"
 )
 
-// NotificationEvent is a NotificationEvent
-type NotificationEvent struct {
-	sourcePID       uint32
-	typeParameter   uint32
-	parameter1      uint32
-	parameter2      uint32
-	stringParameter string
-	nex.Structure
-}
-
-// Bytes encodes the NotificationEvent and returns a byte array
-func (notificationEvent *NotificationEvent) Bytes(stream *nex.StreamOut) []byte {
-	stream.WriteUInt32LE(notificationEvent.sourcePID)
-	stream.WriteUInt32LE(notificationEvent.typeParameter)
-	stream.WriteUInt32LE(notificationEvent.parameter1)
-	stream.WriteUInt32LE(notificationEvent.parameter2)
-	stream.WriteString(notificationEvent.stringParameter)
-
-	return stream.Bytes()
-}
-
 func getFriendNotificationData(err error, client *nex.Client, callID uint32, uiType int32) {
 
 	fmt.Printf("uiType: %d\r\n", uiType)
