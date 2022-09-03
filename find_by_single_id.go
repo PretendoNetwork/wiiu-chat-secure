ba2d7179fed1a3b8b47e7c82bf55651204c6b020
Jonathan Barrow <jonbarrow1998@gmail.com>
2022-09-03 01:05:18 -0400

    One NEX function per file

--- find_by_single_id.go
+++ find_by_single_id.go
@@ -1,35 +1,11 @@
 package main
 
 import (
-	nexproto "github.com/PretendoNetwork/nex-protocols-go"
-
 	nex "github.com/PretendoNetwork/nex-go"
+	nexproto "github.com/PretendoNetwork/nex-protocols-go"
 )
 
-func unregisterGathering(err error, client *nex.Client, callID uint32, idGathering uint32) {
-	endCall(idGathering)
-
-	rmcResponse := nex.NewRMCResponse(nexproto.MatchMakingMethodUnregisterGathering, callID)
-	rmcResponse.SetSuccess(nexproto.MatchMakingProtocolID, []byte{0x01})
-
-	rmcResponseBytes := rmcResponse.Bytes()
-
-	responsePacket, _ := nex.NewPacketV1(client, nil)
-
-	responsePacket.SetVersion(1)
-	responsePacket.SetSource(0xA1)
-	responsePacket.SetDestination(0xAF)
-	responsePacket.SetType(nex.DataPacket)
-	responsePacket.SetPayload(rmcResponseBytes)
-
-	responsePacket.AddFlag(nex.FlagNeedsAck)
-	responsePacket.AddFlag(nex.FlagReliable)
-
-	nexServer.Send(responsePacket)
-}
-
 func findBySingleID(err error, client *nex.Client, callID uint32, id uint32) {
-
 	caller, _, _ := getCallInfoByCaller(id)
 
 	gathering := nexproto.NewGathering()
--- get_session_urls.go
+++ get_session_urls.go
@@ -1,38 +1,9 @@
 package main
 
 import (
-	"fmt"
-
 	nex "github.com/PretendoNetwork/nex-go"
 	nexproto "github.com/PretendoNetwork/nex-protocols-go"
 )
-
-func joinMatchmakeSessionEx(err error, client *nex.Client, callID uint32, gid uint32, strMessage string, dontCareMyBlockList bool, participationCount uint16) {
-	fmt.Printf("gid: %d, strMessage: %s, dontCareMyBlockList: %t, participationCount: %d\r\n", gid, strMessage, dontCareMyBlockList, participationCount)
-
-	endCallRinging(gid)
-
-	output := nex.NewStreamOut(nexServer)
-	output.WriteBuffer(make([]byte, 32))
-
-	rmcResponse := nex.NewRMCResponse(nexproto.MatchmakeExtensionProtocolID, callID)
-	rmcResponse.SetSuccess(nexproto.MatchmakeExtensionMethodJoinMatchmakeSessionEx, output.Bytes())
-
-	rmcResponseBytes := rmcResponse.Bytes()
-
-	responsePacket, _ := nex.NewPacketV1(client, nil)
-
-	responsePacket.SetVersion(1)
-	responsePacket.SetSource(0xA1)
-	responsePacket.SetDestination(0xAF)
-	responsePacket.SetType(nex.DataPacket)
-	responsePacket.SetPayload(rmcResponseBytes)
-
-	responsePacket.AddFlag(nex.FlagNeedsAck)
-	responsePacket.AddFlag(nex.FlagReliable)
-
-	nexServer.Send(responsePacket)
-}
 
 func getSessionUrls(err error, client *nex.Client, callID uint32, gid uint32) {
 	var stationUrlStrings []string
